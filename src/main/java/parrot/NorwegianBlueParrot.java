1d0795b24e508b5e61ddfa1d6ba6a2d38cf2a86c
Maria Soria <maria.it3@gmail.com>
2022-01-17 19:34:05 +0000

    Remove parameter type in NorwegianBlueParrot constructor

--- src/main/java/parrot/NorwegianBlueParrot.java
+++ src/main/java/parrot/NorwegianBlueParrot.java
@@ -2,8 +2,8 @@ package parrot;
 
 public class NorwegianBlueParrot extends Parrot {
 
-    public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(type, numberOfCoconuts, voltage, isNailed);
+    public NorwegianBlueParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
