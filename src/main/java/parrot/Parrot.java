7c27a4cf819f82e20d11585884faeff4beb1867f
Maria Soria <maria.it3@gmail.com>
2022-01-17 19:30:04 +0000

    Move methods to their own classes

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -18,14 +18,6 @@ public class Parrot {
         throw new RuntimeException("Should be unreachable");
     }
 
-    protected double getBaseSpeed(double voltage) {
-        return Math.min(24.0, voltage * getBaseSpeed());
-    }
-
-    protected double getLoadFactor() {
-        return 9.0;
-    }
-
     protected double getBaseSpeed() {
         return 12.0;
     }
