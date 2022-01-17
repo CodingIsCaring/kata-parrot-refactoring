03c57667fb41f2d9443cc44ebd4ba87371137c27
Maria Soria <maria.it3@gmail.com>
2022-01-17 19:18:18 +0000

    Add NorwegianBlueParrot class and override getSpeed method

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -4,8 +4,8 @@ public class Parrot {
 
     private ParrotTypeEnum type;
     protected int numberOfCoconuts;
-    private double voltage;
-    private boolean isNailed;
+    protected double voltage;
+    protected boolean isNailed;
 
     public Parrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
         this.type = type;
@@ -15,15 +15,10 @@ public class Parrot {
     }
 
     public double getSpeed() {
-        switch (type) {
-            case NORWEGIAN_BLUE:
-                return (isNailed) ? 0 : getBaseSpeed(voltage);
-            default:
-                throw new RuntimeException("Should be unreachable");
-        }
+        throw new RuntimeException("Should be unreachable");
     }
 
-    private double getBaseSpeed(double voltage) {
+    protected double getBaseSpeed(double voltage) {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
 
